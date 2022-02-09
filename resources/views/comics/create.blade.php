6fd9320bbf20941ffb7c8736d7268539f7842ece
dominici94 <mattiadominici94@gmail.com>
2022-02-09 18:15:37 +0100

    create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.base')
+
+@section('pageContent')
+<h1>Crea un nuovo fumetto</h1>
+
+<form>
+    <div class="form-group">
+      <label for="title">Titolo</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="inserisci il titolo del fumetto">
+    </div>
+
+    <div class="form-group">
+      <label for="description">Descrizione</label>
+      <textarea class="form-control" id="description" name="description" placeholder="inserisci la descrizione del fumetto" rows="6"></textarea>
+    </div>
+
+    <div class="form-group">
+        <label for="image">Immagine</label>
+        <input type="text" class="form-control" id="image" name="image" placeholder="inserisci l'URL dell'immagine">
+    </div>
+
+    <div class="form-group">
+        <label for="price">Prezzo</label>
+        <input type="number" class="form-control" step="0.01" name="price" id="price" placeholder="inserisci il prezzo del fumetto">
+    </div>
+
+    <div class="form-group">
+        <label for="series">Serie</label>
+        <input type="text" class="form-control" name="series" id="series" placeholder="inserisci la serie del fumetto">
+    </div>
+
+    <div class="form-group">
+        <label for="sale">Data di vendita</label>
+        <input type="date" class="form-control" name="sale_dat" id="sale" placeholder="inserisci la data di vendita">
+    </div>
+
+    <div class="form-group">
+        <label for="type">Genere</label>
+        <select class="form-control" name="type" id="type">
+            <option value="graphic novel">graphic novel</option>
+            <option value="comic book">comic book</option>
+            <option value="other">altro</option>
+        </select>
+
+    </div>
+
+    <button type="submit" class="btn btn-primary">Crea Comic</button>
+</form>
+@endsection
