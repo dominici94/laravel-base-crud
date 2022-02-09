458df739f91a0d92032957ee2c17431ef542f26f
dominici94 <mattiadominici94@gmail.com>
2022-02-09 16:34:34 +0100

    installazione bootstrap

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('pageContent')
+    <ul>
+        @foreach ($comics as $comic)
+            <li>{{$comic['title']}}</li>
+        
+        @endforeach
+    </ul>
+@endsection
